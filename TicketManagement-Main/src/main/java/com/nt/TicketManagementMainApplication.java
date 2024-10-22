055c66ad6e2b9596348309fa1ece3a4b67c55517
akhis <akhis@SAIMAHESH>
2024-10-22 14:44:32 +0530

    kusuma saved

--- TicketManagement-Main/src/main/java/com/nt/TicketManagementMainApplication.java
+++ TicketManagement-Main/src/main/java/com/nt/TicketManagementMainApplication.java
@@ -9,6 +9,7 @@ public class TicketManagementMainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketManagementMainApplication.class, args);
+		//kusuma hhjhjh
 	}
 
 }
